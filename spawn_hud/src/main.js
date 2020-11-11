24e8c8b4c6af966db17d04305ed19f9fd264e96a
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-11 10:28:17 -0300

    -- add buefy dependencies

--- spawn_hud/src/main.js
+++ spawn_hud/src/main.js
@@ -1,12 +1,25 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Nui from './utils/Nui';
-import { Message } from 'buefy'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
-Vue.use(Message)
+Vue.use(Buefy, {
+  defaultIconPack: 'fas',
+  defaultIconComponent: 'font-awesome-icon'
+});
 
 Vue.config.productionTip = false;
+
 
 new Vue({
   render: h => h(App),
