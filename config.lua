11f9b7c51034398378aecfa70fd7ca95489dc391
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-11 10:30:09 -0300

    -- add icon

--- config.lua
+++ config.lua
@@ -2,9 +2,10 @@ config = {}
 config.Coords = {}
 
 config.Coords.vinewood  = { 
-    ["x"] = 294.50, 
-    ["y"] = 178.42,
-    ["z"] = 104.24,
-    ["heading"] = 50.2,
-    ["label"] = 'Vinewood'
+    ['x'] = 294.50, 
+    ['y'] = 178.42,
+    ['z'] = 104.24,
+    ['heading'] = 50.2,
+    ['label'] = 'Vinewood',
+    ['icon'] = 'user'
 }
