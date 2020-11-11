8e9384ff11fd94e2f9c7e9e087fb1053526cb626
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-10 21:11:21 -0300

    -- add ui page on fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Lucian Fialho'
+description 'This is a spawn interface for esx'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+ui_page 'spawn_hud/dist/index.html'
+
+files {
+	'spawn_hud/dist/index.html',
+}
+
+dependency 'es_extended'
