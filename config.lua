dafccd804f30fdeeee6548ea0f89d976bf489273
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-11 09:33:12 -0300

    -- add heading

--- config.lua
+++ config.lua
@@ -2,8 +2,9 @@ config = {}
 config.Coords = {}
 
 config.Coords.vinewood  = { 
-    ["x"] = 294.50 , 
-    ["y"] = 178.42, 
+    ["x"] = 294.50, 
+    ["y"] = 178.42,
     ["z"] = 104.24,
+    ["heading"] = 50.2,
     ["label"] = 'Vinewood'
 }
